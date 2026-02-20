a7714fca2bc46ad4cbf25b5532d2ca9324663e64
OisinB26 <151824602+OisinB26@users.noreply.github.com>
2026-02-20 15:33:00 +0000

    Add constructor to LifeTable for mortality rates
    Initialize LifeTable class with annual mortality rates.

--- src/life_table.py
+++ src/life_table.py
@@ -1,2 +1,8 @@
 class LifeTable:
-    pass
+        
+    def __init__(self, qx):
+        """
+        qx = list of annual mortality rates
+        Example: [0.01, 0.012, 0.013]
+        """
+        self.qx = qx
