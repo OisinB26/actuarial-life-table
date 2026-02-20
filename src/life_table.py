7d7bd050fc7b4d7317aded704030b435fac980fc
OisinB26 <151824602+OisinB26@users.noreply.github.com>
2026-02-20 15:34:06 +0000

    Add calculation for annual survival probabilities
    Calculate the probability of surviving each year based on mortality rates.

--- src/life_table.py
+++ src/life_table.py
@@ -6,3 +6,7 @@ class LifeTable:
         Example: [0.01, 0.012, 0.013]
         """
         self.qx = qx
+
+        # px is the probability of surviving each year
+        self.px = [1 - q for q in qx]
+            
